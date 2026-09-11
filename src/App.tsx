6345a7809678f787e04ad2d6662095ac8a3d526b
rohanislambd <coderootbdofficial24@gmail.com>
2026-09-11 19:34:02 +0600

    setup assignment-5 DevStack with React, Tailwind, DaisyUI and React Icons

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { HiAcademicCap } from "react-icons/hi"
+
+function App() {
+
+  return (
+    <>
+    <h2 className="text-4xl">DevStack</h2>
+    <button className="btn btn-outline">Default</button>
+
+      <HiAcademicCap />
+
+    </>
+  )
+}
+
+export default App
